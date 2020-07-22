7f1c22f58ea4a7ae73377c4e675a3904100f44d3
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-22 20:49:51 +0800

    OO [Eric] feature: finish the pratice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,46 @@
 package practice10;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> members = new ArrayList<Student>();
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student) {
+        if (this.members.contains(student)) {
+            this.leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        this.members.add(student);
+    }
+
+    public boolean isIn(Student student) {
+        if (this.members.contains(student)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,22 @@
 package practice10;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        if (this.klass.getLeader() == null || this.klass.getLeader().getId() != this.getId()) {
+            return super.introduce() + " I am a Student. I am at Class " + this.klass.getNumber() + ".";
+        } else {
+            return super.introduce() + " I am a Student. I am Leader of Class " + this.klass.getNumber() + ".";
+        }
+    }
 }
