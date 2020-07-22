c5340714a0ff7f853fd010956982697782192b51
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-22 19:26:48 +0800

    OO [Eric] feature: finish the pratice07

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,18 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + this.klass.getNumber() + ".";
+    }
 }
