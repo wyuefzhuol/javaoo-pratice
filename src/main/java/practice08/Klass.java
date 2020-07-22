1d5f09b03406a47c33b305f25f3ee86e11d7b8ab
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-22 19:57:20 +0800

    OO [Eric] feature: finish the pratice08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,26 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,39 @@
 package practice08;
 
 public class Person {
+    protected int id;
+    protected String name;
+    protected int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Person person = (Person) obj;
+        if (person.getId() == this.id) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
