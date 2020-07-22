4a411129fadf95e60fd179425bde75147b1273d3
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-22 20:26:51 +0800

    OO [Eric] feature: finish the pratice09

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,34 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        if (this.klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + this.klass.getNumber() + ".";
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == this.klass.getNumber()) {
+            return super.introduce() + " I am a Teacher. I teach " + student.getName() + ".";
+        } else {
+            return super.introduce() + " I am a Teacher. I don't teach " + student.getName() + ".";
+        }
+    }
 }
