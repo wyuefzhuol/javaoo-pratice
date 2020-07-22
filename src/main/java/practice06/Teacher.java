ef1d7fc48f5664dee2dfc4227a5e00c819e5e3b4
ERIC WANG (ITA-ISDC-ISD-OOCLL/ZHA) <WANGER3@OOCL.COM>
2020-07-22 17:39:26 +0800

    OO [Eric] feature: finish the pratice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if ("0".equals(String.valueOf(this.klass)) || "null".equals(String.valueOf(this.klass))) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            return super.introduce() + " I am a Teacher. I teach Class " + this.klass + ".";
+        }
+    }
 }
